c25c3293ec7479d8bd88451e700580f86aebca33
stan phillips <lunchbox9037@gmail.com>
2021-02-12 14:20:43 -0500

    mostly working randomization and persistence. minor ui bug

--- PairRandomizer/PairRandomizer/Controllers/Model Controller/PersonController.swift
+++ PairRandomizer/PairRandomizer/Controllers/Model Controller/PersonController.swift
@@ -5,12 +5,13 @@
 //  Created by stanley phillips on 2/12/21.
 //
 
-import Foundation
+import CoreData
 
 class PersonController {
     // MARK: - Properties
     static let shared = PersonController()
     var people: [Person] = []
+    var pairs: [[Person]] = []
     
     // MARK: - CRUD
     func addPerson(name: String) {
@@ -24,13 +25,68 @@ class PersonController {
         saveToPersistentStorage()
     }
     
+    func randomize() {
+        people = people.shuffled()
+        pairs = [[]]
+        var pairsCount = 0
+        if people.count % 2 == 0 {
+            pairsCount = people.count / 2
+        } else {
+            pairsCount = (people.count / 2) + 1
+        }
+        
+        var i = 0
+        var n = 0
+        for _ in 0..<pairsCount {
+            pairs.append([])
+        }
+        
+        for p in 0..<pairsCount {
+            while n < 2 && i < people.count {
+                pairs[p].append(people[i])
+                i += 1
+                n += 1
+            }
+            n = 0
+        }
+        
+        saveToPersistentStorage()
+    }
+    
+    func reSectionWhenPersisting() {
+        pairs = [[]]
+        var pairsCount = 0
+        if people.count % 2 == 0 {
+            pairsCount = people.count / 2
+        } else {
+            pairsCount = (people.count / 2) + 1
+        }
+        
+        var i = 0
+        var n = 0
+        for _ in 0..<pairsCount {
+            pairs.append([])
+        }
+        
+        for p in 0..<pairsCount {
+            while n < 2 && i < people.count {
+                pairs[p].append(people[i])
+                i += 1
+                n += 1
+            }
+            n = 0
+        }
+        
+        saveToPersistentStorage()
+    }
+    
     // MARK: - Persistence
     private func fileURL() -> URL {
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let documentsDirectoryURL = urls[0].appendingPathComponent("PairRandomizer.json")
         return documentsDirectoryURL
     }
-    
+
     func saveToPersistentStorage() {
         do {
             let data = try JSONEncoder().encode(people)
@@ -40,11 +96,11 @@ class PersonController {
             print(error.localizedDescription)
         }
     }
-    
+
     func loadFromPersistentStorage() {
         do {
             let data = try Data(contentsOf: fileURL())
-            people = try JSONDecoder().decode([Person].self, from: data)
+           people = try JSONDecoder().decode([Person].self, from: data)
         } catch {
             print(error)
             print(error.localizedDescription)
--- PairRandomizer/PairRandomizer/Controllers/View Controller/PairListTableViewController.swift
+++ PairRandomizer/PairRandomizer/Controllers/View Controller/PairListTableViewController.swift
@@ -12,9 +12,11 @@ class PairListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         PersonController.shared.loadFromPersistentStorage()
-//        tableView.reloadData()
+        PersonController.shared.reSectionWhenPersisting()
+        tableView.reloadData()
     }
     
+    // MARK: - Actions
     @IBAction func addButtonTapped(_ sender: Any) {
         let alert = UIAlertController(title: "Add Person", message: nil, preferredStyle: .alert)
         //add a text field to the alert
@@ -24,6 +26,7 @@ class PairListTableViewController: UITableViewController {
         let addButton = UIAlertAction(title: "Add", style: .default) { (action) in
             guard let personName = alert.textFields?[0].text, !personName.isEmpty else {return}
             PersonController.shared.addPerson(name: personName)
+            PersonController.shared.randomize()
             self.tableView.reloadData()
         }
         
@@ -33,32 +36,37 @@ class PairListTableViewController: UITableViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    @IBAction func randomizeButtonTapped(_ sender: Any) {
+        PersonController.shared.randomize()
+        tableView.reloadData()
+    }
+    
     // MARK: - Table view data source
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        return 0
-//    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return PersonController.shared.pairs.count
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return PersonController.shared.people.count
+        return PersonController.shared.pairs[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Group \(section + 1)"
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
-        cell.textLabel?.text = PersonController.shared.people[indexPath.row].name.capitalized
+        cell.textLabel?.text = PersonController.shared.pairs[indexPath.section][indexPath.row].name.capitalized
         return cell
     }
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let personToDelete = PersonController.shared.people[indexPath.row]
+            let personToDelete = PersonController.shared.pairs[indexPath.section][indexPath.row]
+            PersonController.shared.pairs[indexPath.section].remove(at: indexPath.row)
             PersonController.shared.delete(person: personToDelete)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.reloadData()
         }
     }
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-    }
-    */
 }//end class
